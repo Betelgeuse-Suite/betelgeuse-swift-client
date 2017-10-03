9f9c6ddf44eb4396fc1dc7926864fcc87b5003b9
Gabriel C. Troia <catalin.troia@gmail.com>
2017-10-03 19:20:07 -0400

    Create a Swift framework

---
+++ BetelgeuseSwiftSDK/BetelgeuseSwiftSDK.h
@@ -0,0 +1,19 @@
+//
+//  BetelgeuseSwiftSDK.h
+//  BetelgeuseSwiftSDK
+//
+//  Created by gabriel troia on 10/3/17.
+//  Copyright © 2017 Betelgeuse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BetelgeuseSwiftSDK.
+FOUNDATION_EXPORT double BetelgeuseSwiftSDKVersionNumber;
+
+//! Project version string for BetelgeuseSwiftSDK.
+FOUNDATION_EXPORT const unsigned char BetelgeuseSwiftSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BetelgeuseSwiftSDK/PublicHeader.h>
+
+
