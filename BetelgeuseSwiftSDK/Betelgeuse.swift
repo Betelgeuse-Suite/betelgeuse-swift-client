3e951de6267a1496376b1b1a980861b7bbc57dc7
Gabriel C. Troia <catalin.troia@gmail.com>
2017-10-07 18:43:47 -0400

    Add ability to determine the best vesion

--- BetelgeuseSwiftSDK/Betelgeuse.swift
+++ BetelgeuseSwiftSDK/Betelgeuse.swift
@@ -8,13 +8,65 @@
 
 import Foundation
 
+class Version {
+    private let major: Int
+    private let minor: Int
+    private let patch: Int
+
+    init(fromString: String) {
+        let fromArray = fromString.components(separatedBy: ".")
+
+        // Add validation
+
+        self.major = Int(fromArray[0]) ?? -1
+        self.minor = Int(fromArray[1]) ?? -1
+        self.patch = Int(fromArray[2]) ?? -1
+    }
+
+    public func toString() -> String {
+        return "\(major).\(minor).\(patch)"
+    }
+
+    public static func sortDesc(_ versions: [Version]) -> [Version] {
+        return versions.sorted(by: {a, b in
+            return isNewerThan(b, a)
+        })
+    }
+
+    public static func isNewerThan(_ a: Version, _ b: Version) -> Bool {
+        return b.major > a.major
+            || b.major == a.major && b.minor > a.minor
+            || b.major == a.major && b.minor == a.minor && b.patch > a.patch
+    }
+
+    public static func isNonBreakingRelease(_ a: Version, _ b: Version) -> Bool {
+        return a.major == b.major
+    }
+
+    public static func getBestVersion(currentVersion: Version, versions: [Version]) -> Version? {
+        let bestVersions = sortDesc(onlyNonBreakingAndNewer(than: currentVersion, versions))
+
+        if bestVersions.count > 0 {
+            return bestVersions[0]
+        }
+
+        return nil
+    }
+
+    public static func onlyNonBreakingAndNewer(than version: Version, _ versions: [Version]) -> [Version] {
+        return versions.filter({ nextVersion -> Bool in
+            return isNonBreakingRelease(version, nextVersion) && isNewerThan(version, nextVersion)
+        });
+    }
+}
+
 public class Betelgeuse {
     private let localDataBundleUrl: URL
     private let localFileName: String
     private let localFileExtension: String
     private let remoteDataUrl: URL
     private let versionsRegisterURL: URL
-    private let currentVersion: String // Version type
+    private let currentVersion: Version // Version type
 
     public init(
         localDataBundleUrl: URL,
@@ -29,7 +81,34 @@ public class Betelgeuse {
         self.localFileExtension = localFileExtension
         self.remoteDataUrl = remoteDataUrl
         self.versionsRegisterURL = versionsRegisterUrl
-        self.currentVersion = currentVersion // todo -> Version Type
+        self.currentVersion = Version(fromString: currentVersion) // todo -> Version Type
+
+        let vv = [
+            Version(fromString: "0.0.1"),
+            Version(fromString: "2.0.1"),
+            Version(fromString: "0.0.2"),
+            Version(fromString: "1.5.2"),
+            Version(fromString: "0.1.3"),
+            Version(fromString: "1.0.0"),
+            Version(fromString: "1.0.1"),
+            Version(fromString: "1.0.2"),
+        ]
+
+        print("v \(self.currentVersion.toString())")
+
+        let x = Version.sortDesc(vv).map({ v in
+            return v.toString()
+        })
+
+        if let bestVersion = Version.getBestVersion(currentVersion: Version(fromString: "1.0.0"), versions: vv) {
+            print("Best Version: \(bestVersion.toString())")
+        }
+        else {
+            print("no best version yet")
+        }
+
+
+//        print(x)
 
         //        this.getAllVersions(completionHandler: ({ data in
         //            print(data.allKeys)
