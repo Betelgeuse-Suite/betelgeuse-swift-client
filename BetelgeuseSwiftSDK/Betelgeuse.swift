168fc349f7c9f432c0d01f32bc0f0d41f4796f21
Gabriel C. Troia <catalin.troia@gmail.com>
2017-10-07 20:03:09 -0400

    Clean up the code a bit.

--- BetelgeuseSwiftSDK/Betelgeuse.swift
+++ BetelgeuseSwiftSDK/Betelgeuse.swift
@@ -6,6 +6,18 @@
 //  Copyright © 2017 Betelgeuse. All rights reserved.
 //
 
+
+// Steps Left
+
+// 
+// Load the next version and save it locally for the next usage
+// Determine what event triggers the checkFor Updates/Install methods
+// Determine where to store the data
+//   Look into:
+//    https://stackoverflow.com/questions/28628225/how-to-save-local-data-in-a-swift-app
+//    http://www.raywenderlich.com/85578/first-core-data-app-using-swift
+//
+
 import Foundation
 
 class Version {
@@ -64,7 +76,8 @@ public class Betelgeuse {
     private let localDataBundleUrl: URL
     private let localFileName: String
     private let localFileExtension: String
-    private let remoteDataUrl: URL
+    private let remoteDataBaseUrl: URL
+    private let remoteDataPath: String
     private let versionsRegisterURL: URL
     private let currentVersion: Version // Version type
 
@@ -72,39 +85,51 @@ public class Betelgeuse {
         localDataBundleUrl: URL,
         localFileName: String,
         localFileExtension: String,
-        remoteDataUrl: URL,
+        remoteDataBaseUrl: URL,
+        remoteDataPath: String,
         versionsRegisterUrl: URL,
         currentVersion: String) {
 
         self.localDataBundleUrl = localDataBundleUrl
         self.localFileName = localFileName
         self.localFileExtension = localFileExtension
-        self.remoteDataUrl = remoteDataUrl
+        self.remoteDataBaseUrl = remoteDataBaseUrl
+        self.remoteDataPath = remoteDataPath
         self.versionsRegisterURL = versionsRegisterUrl
         self.currentVersion = Version(fromString: currentVersion) // todo -> Version Type
 
+        checkForUpdates(completionHandler: { bestVersion in
+            self.loadData(version: bestVersion, completionHandler: { (data) in
+                print("json")
+                print(data)
+            })
+        })
+    }
+
+    private func checkForUpdates(completionHandler: @escaping(Version) -> Void) {
         getAllVersions(completionHandler: ({ data in
             let allVersions = data.allKeys.map({ s in
                 return Version(fromString: s as! String)
             })
 
-            Version.sortDesc(allVersions).forEach({ (v) in
-                print(v.toString())
-            })
-
             if let bestVersion = Version.getBestVersion(currentVersion: self.currentVersion, versions: allVersions) {
-                print("Best Version: \(bestVersion.toString())")
+                print("Best Version found: \(bestVersion.toString())")
+
+                completionHandler(bestVersion)
             }
             else {
-                print("No unbreaking changes on your current version \(currentVersion)")
+                print("No unbreaking changes on your current version \(self.currentVersion)")
             }
-
         }))
-//        BetelgeuseSwiftSDK.loadUrl()
-//
-//        if let m = BetelgeuseSwiftSDK.loadDataFromFile() {
-//            print("The Model() is \(m.nested.nested.file.value)")
-//        }
+    }
+
+    private func loadData(version: Version, completionHandler: @escaping(NSDictionary) -> Void) {
+        if let url = URL(string: "\(self.remoteDataBaseUrl)/v\(version.toString())/\(self.remoteDataPath)") {
+            self.loadData(fromUrl: url, completionHandler: completionHandler)
+        }
+        else {
+            print("Bad Url")
+        }
     }
 
     //    public static func getModel() -> Model? {
@@ -160,18 +185,22 @@ public class Betelgeuse {
         return nil
     }
 
-    public func getAllVersions(completionHandler: @escaping(NSDictionary) -> Void) {
-        //        let url = URL(string: "\(ENDPOINT_URL)/versions.json")
-        print("Loading all versions from \(self.versionsRegisterURL)")
-        URLSession.shared.dataTask(with: self.versionsRegisterURL, completionHandler: {(data, response, error) in
+    private func getAllVersions(completionHandler: @escaping(NSDictionary) -> Void) {
+        loadData(fromUrl: versionsRegisterURL, completionHandler: completionHandler)
+    }
+
+    private func loadData<T>(fromUrl: URL, completionHandler: @escaping(T) -> Void) {
+        print("Loading data from \(fromUrl.absoluteURL)")
+
+        URLSession.shared.dataTask(with: fromUrl, completionHandler: {(data, response, error) in
             guard let data = data, error == nil else {
-//                print(error)
+                //                print(error)
                 return
             }
             if let jsonResult = try? JSONSerialization.jsonObject(
                 with: data,
                 options: JSONSerialization.ReadingOptions.mutableContainers
-                ) as! NSDictionary {
+                ) as! T {
                 return completionHandler(jsonResult)
             } else {
                 print("Error in reading the json")
